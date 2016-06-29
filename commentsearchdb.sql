c1cd915f9d15258bc351700880bf97b70dca97db
Andrew Harris <harris.1305@gmail.com>
2016-06-29 16:26:47 -0400

    updated sql to not set auto_increment starting val, made id column bigint

--- commentsearchdb.sql
+++ commentsearchdb.sql
@@ -1,10 +1,11 @@
-create database commentsearchdb;
-
+create database if not exists commentsearchdb;
 use commentsearchdb;
 
+drop table if exists comments;
 CREATE TABLE `comments` (
-  `comment_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `comment_id` bigint unsigned NOT NULL AUTO_INCREMENT,
   `comment_text` longtext,
   PRIMARY KEY (`comment_id`),
   FULLTEXT KEY `comment_text` (`comment_text`)
-) ENGINE=MyISAM AUTO_INCREMENT=201908 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
